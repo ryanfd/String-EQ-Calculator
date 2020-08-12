678782ff57f527d581dac01fec00587a5df174c2
ryan <rdonnell@sfu.ca>
2020-08-12 15:36:53 -0700

    need proper string conversion

--- recursiveFuncs.hs
+++ recursiveFuncs.hs
@@ -1,3 +1,5 @@
+import System.Environment -- for main arguments
+
 hailstone::Integer->Integer
 hailstone n
  | n `mod` 2 == 0    = n `div` 2
@@ -12,3 +14,22 @@ fib :: Int -> Int
 fib 0 = 0
 fib 1 = 1
 fib x = (fib (x-1)) + (fib (x-2))
+
+divisors :: Int -> [Int]
+divisors n = [i | i <- [2..(n `div` 2)], n `mod` i == 0]
+
+primes :: Int -> [Int]
+primes n = [i | i <- [2..n], divisors i == []]
+
+fact'::Integer->Integer
+fact' n = foldl (*) 1 [1..n]
+
+toInt :: String -> Integer
+toInt s = read s :: Integer
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of ["hail", n] -> toInt n
+                   
+  print args
