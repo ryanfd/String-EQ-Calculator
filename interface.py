72e50daac3c276f398328eb910a28b77185e60d8
ryan <rdonnell@sfu.ca>
2020-08-12 14:37:49 -0700

    c++ communication is functional

--- interface.py
+++ interface.py
@@ -1,47 +1,25 @@
-import ctypes
+import parser
 
-# c++ methods
-parser = ctypes.CDLL('./parser.so')
-parser.doCalculation.argtypes = [ctypes.c_wchar_p]
-parser.doCalculation.restype = ctypes.c_float
-
-
-# def ctypes_removeSpaces(a):
-#     return parser.removeSpaces(ctypes.c_wchar_p(a))
-
-def ctypes_doCalculation(a):
-    return parser.doCalculation(ctypes.c_wchar_p(a))
-
-
-
-# libfoo = ctypes.CDLL('libfoo.so')
-
-# libfoo.Foo_new.argtypes = [ctypes.c_int]
-# libfoo.Foo_new.restype = ctypes.c_void_p
-
-# libfoo.Foo_bar.argtypes = [ctypes.c_void_p]
-# libfoo.Foo_bar.restype = ctypes.c_int
-
-# class Foo:
-#     def __init__(self, bar):
-#         self.foo = libfoo.Foo_new(bar)
-
-#     def bar(self):
-#         return libfoo.Foo_bar(self.foo)
-
-# import parser
-
-
+goAgain = "y"
 
 print("\n\n***STRING EQUALITY CALCULATOR***\n")
 
-print("Welcome to the Polyglot Interface!\n")
+while (goAgain == "y") :
+    print("Welcome to the Polyglot Interface!\n")
 
-print("VALID OPERATORS: { '^', '*', '/', '+', '-' }\n")
+    print("VALID OPERATORS: { '^', '*', '/', '+', '-' }\n")
 
-# get equation
-inputValue = str(input("Please enter an equation using the '^', '*', '/', '+', or '-' operators:\n"))
-print("Your equation is: ", inputValue, " with a type of ", type(inputValue))
-# print("answer: ", ctypes_doCalculation(inputValue))
-# result = ctypes_doCalculation(inputValue)
-# parser.removeSpaces(inputValue)
+    # get equation
+    print("PLEASE DO NOT INPUT ANY SPACES OR TABS")
+    inputValue = str(input("Please enter an equation using the '^', '*', '/', '+', or '-' operators: "))
+    print("\n\nYour equation is: ", inputValue, " with a type of ", type(inputValue))
+
+    print("Your answer is = ", parser.doCalculation(inputValue))
+
+    # Allow multiple uses...
+    goAgain = input("Would you like to go again? Y/n?")
+
+    if (goAgain.lower() != "y") : 
+        break
+
+print("\n\n***Thanks for using the String Equality Calculator!***\n\n")
